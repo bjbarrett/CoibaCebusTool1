3a7aa6b5b0e0674bf3b306908081db74fa49a5f0
Brendan Barrett <bbarrett@orn.mpg.de>
2018-08-01 15:50:59 -0700

    code for calcs of group size perry et al 2012

---
+++ Cebus_group_size_analysis_from Perryetal2012.R
@@ -0,0 +1,8 @@
+#calculated maximum annual group size from data in figure  Figure 7.1 in Perry Godoy and Lammers. The Lomas Barbudal Monkey Project: Two Decades of Research on Cebus capucinus. 2012.
+
+x <- c(16,20,21,22,25,31,28,30,26,29,31,35,39,37,20,21,24,24,25,26,27,10,14,16,18,21,21,17,16,27,31,31,32,31,35,39,40,37,26,28,32,32,34,31,13,17,21,16,17,18,19,5,7,6,8,8,8,7,9,12,8,17,19,19,26,26,30,34,35,36,39,38,20,21,21,15,19,21,22)
+
+mean(x)
+min(x)
+max(x)
+sd(x)
